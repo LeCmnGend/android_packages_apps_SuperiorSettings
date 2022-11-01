4078f5dbaf3b189f87c6225342379655111c535e
LeCmnGend <namhoang1235@gmail.com>
2022-11-01 21:16:53 +0700

    =============================FOD: Kang from Syberia OS

---
+++ src/com/superior/settings/fragments/FodTweaks.java
@@ -0,0 +1,42 @@
+/*
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.superior.settings.fragments;
+
+import android.os.Bundle;
+import android.content.Context;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.internal.logging.nano.MetricsProto;
+
+import androidx.preference.PreferenceScreen;
+import androidx.preference.PreferenceCategory;
+
+import com.superior.settings.utils.Utils;
+
+public class FodTweaks extends SettingsPreferenceFragment {
+
+    private static final String FOD_ANIMATION_CATEGORY = "fod_animations";
+
+    @Override
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        addPreferencesFromResource(R.xml.superior_fod_tweaks);
+        final PreferenceScreen prefScreen = getPreferenceScreen();
+        final PreferenceCategory fodCat = (PreferenceCategory) prefScreen
+                .findPreference(FOD_ANIMATION_CATEGORY);
+        final boolean isFodAnimationResources = Utils.isPackageInstalled(getContext(),
+                      getResources().getString(com.android.internal.R.string.config_fodAnimationPackage));
+        if (!isFodAnimationResources) {
+            prefScreen.removePreference(fodCat);
+        }
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.SUPERIOR;
+    }
+}
